239b3258323ad531d5e188dc3614b0c3bb56dc23
Phạm Minh Triết <142817355+trietpham252@users.noreply.github.com>
2023-10-26 22:00:19 +0700

    update

--- src/index.js
+++ src/index.js
@@ -29,7 +29,6 @@ mongoose.connect(`${process.env.MONGO_DB}`)
     .catch((err) => {
         console.error('Error connecting to the database:', err)
     })
-
 app.listen(port, () => {
     console.log('Server is running on port:', port)
 })
